e9d3383f0ed42342d892f7cdcb87353f52eaa1d9
Whatcott, Michael <mdwhatcott@gmail.com>
2015-11-06 00:09:18 -0700

    Reset. Ready to implement.

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -1,146 +1,25 @@
 package controller
 
-import (
-	"strings"
-
-	"github.com/smartystreets/assertions/should"
-	"github.com/smartystreets/gunit"
-)
-
-//go:generate gunit
-
-type Fixture struct {
-	*gunit.Fixture
-	hardware   *FakeHardware
-	controller *Controller
-}
-
-func (this *Fixture) Setup() {
-	this.hardware = NewFakeHardware()
-	this.hardware.SetHeater(true)
-	this.hardware.SetBlower(true)
-	this.hardware.SetCooler(true)
-	this.controller = New(this.hardware)
-	this.controller.Calibrate(70, 5, 10)
-}
-func (this *Fixture) assertState(expected string) {
-	this.So(this.hardware.State(), should.Equal, expected)
-}
-func (this *Fixture) assertAllOff() {
-	this.assertState("")
-}
-
-func (this *Fixture) TestAllHardwareOffAtStartup() {
-	this.assertAllOff()
-}
-
-func (this *Fixture) TestNothingTurnsOnIfTemperatureJustRight() {
-	this.hardware.current = 70
-	this.controller.Regulate()
-	this.assertAllOff()
-}
-
-func (this *Fixture) TestTurnOnCoolerAndBlowerIfTooHot() {
-	this.hardware.current = 76
-	this.controller.Regulate()
-	this.assertState("BLOWING COOLING")
-}
-
-func (this *Fixture) TestTurnOnHeaterAndBLowerIfTooCold() {
-	this.hardware.current = 64
-	this.controller.Regulate()
-	this.assertState("HEATING BLOWING")
-}
-
-func (this *Fixture) TestAlarms() {
-	for x := 0; x < 60; x++ {
-		this.assertAlarm(x, "COLD!")
-	}
-	for x := 60; x <= 80; x++ {
-		this.assertAlarm(x, "")
-	}
-	for x := 81; x <= 130; x++ {
-		this.assertAlarm(x, "HOT!")
-	}
-}
-func (this *Fixture) assertAlarm(temperature int, expected string) {
-	this.Setup()
-	this.hardware.current = temperature
-	this.controller.Regulate()
-	this.So(this.hardware.Alarms(), should.Equal, expected)
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-type FakeHardware struct {
-	current   int
-	heating   bool
-	cooling   bool
-	blowing   bool
-	coldAlarm bool
-	heatAlarm bool
-}
-
-func NewFakeHardware() *FakeHardware { return &FakeHardware{} }
-
-func (this *FakeHardware) AmbientTemperature() int { return this.current }
-func (this *FakeHardware) SetHeater(on bool)       { this.heating = on }
-func (this *FakeHardware) SetCooler(on bool)       { this.cooling = on }
-func (this *FakeHardware) SetBlower(on bool)       { this.blowing = on }
-func (this *FakeHardware) SetColdAlarm(on bool)    { this.coldAlarm = on }
-func (this *FakeHardware) SetHeatAlarm(on bool)    { this.heatAlarm = on }
-func (this *FakeHardware) IsHeating() bool         { return this.heating }
-func (this *FakeHardware) IsCooling() bool         { return this.cooling }
-func (this *FakeHardware) IsBlowing() bool         { return this.blowing }
-func (this *FakeHardware) ColdAlarm() bool         { return this.coldAlarm }
-func (this *FakeHardware) HeatAlarm() bool         { return this.heatAlarm }
-
-func (this *FakeHardware) State() string {
-	state := ""
-	if this.IsHeating() {
-		state += "HEATING "
-	}
-	if this.IsBlowing() {
-		state += "BLOWING "
-	}
-	if this.IsCooling() {
-		state += "COOLING "
-	}
-	return strings.TrimSpace(state)
-}
-func (this *FakeHardware) Alarms() string {
-	state := ""
-	if this.HeatAlarm() {
-		state += "HOT! "
-	}
-	if this.ColdAlarm() {
-		state += "COLD!"
-	}
-	return strings.TrimSpace(state)
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////
-
 /*
-Test Cases:
-1. On startup everything is off
-3. turnOnCoolerAndBlowerIfTooHot
-4. turnOnHeaterAndBlowerIfTooCold
-2a. nothingTurnsOnIfTempJustRight
-5. turnOnHeatAlarmAtThreshold
-6. turnOnColdAlarmAtThreshold
-2b. noAlarmReturnedIfTempNominal
-7. NoAlarmReturnedWhenHighTempReturnsToNominal
-8. NoAlarmReturnedWhenLowTempReturnsToNominal
-9. heaterTurnsOffButBlowerRemainsOnFor5MinutesAfterHeating
-10. coolerTurnsOffIfHotAgain
-11. coolerMustRemainOffForThreeMinutesBeforeRestart
-12. coolerDoesNotTurnOnAfterDelayIfNotNeeded
-13. blowerDelaysAfterFurnaceGoesOff (necessary?)
-14. blowerStaysOnWhenCoolingAfterHeating
+Basic Logic:
+1. on startup everything is off
+2. turn on cooler and blower if too hot
+3. turn on heater and blower if too cold
+4. nothing turns on if temp just right
+
+Advanced Logic:
+5. heater turns off but blower remains on for 5 minutes after heating
+6. cooler turns off if hot again
+7. cooler must remain off for three minutes before restart
+8. cooler does not turn back on after delay if not needed
+9. blower stays on when cooling after heating
+
+Alarm Logic:
+10. turn on heat alarm at threshold
+11. turn on cold alarm at threshold
+12. no alarm returned if temp nominal
+13. no alarm returned when high temp returns to nominal
+14. no alarm returned when low temp returns to nominal
 */
 
 /*
